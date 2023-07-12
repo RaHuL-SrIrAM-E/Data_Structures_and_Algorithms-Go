580554af14b37e4b713c0ca36b54c7f45b43e548
Rahul Sriram Esakkimuthu <81294511+RaHuL-SrIrAM-E@users.noreply.github.com>
2023-07-12 12:38:28 +0400

    Create InsertionSort.go

---
+++ InsertionSort.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func InsertionSort(array []int) {
+	for itemIndex, itemValue := range array {
+		for itemIndex != 0 && array[itemIndex-1] > itemValue {
+			array[itemIndex] = array[itemIndex-1]
+			itemIndex -= 1
+		}
+		array[itemIndex] = itemValue
+	}
+}
