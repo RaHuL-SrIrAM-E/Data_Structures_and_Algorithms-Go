d173030aab222e81fd8893136b22b54e02271fcf
Rahul Sriram Esakkimuthu <81294511+RaHuL-SrIrAM-E@users.noreply.github.com>
2023-07-12 12:40:18 +0400

    Create SelectionSort.go

---
+++ SelectionSort.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func SelectionSort(array []int) {
+	for arrayIndex := range array {
+		minValue := array[arrayIndex]
+		minIndex := arrayIndex
+		for subArrayIndex := arrayIndex + 1; subArrayIndex < len(array); subArrayIndex++ {
+			if array[subArrayIndex] < minValue {
+				minValue = array[subArrayIndex]
+				minIndex = subArrayIndex
+			}
+		}
+		array[minIndex], array[arrayIndex] = array[arrayIndex], array[minIndex]
+	}
+}
